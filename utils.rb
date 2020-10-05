082cff5461ca4d7fb65f6d37394f1983e99bc145
wangqi80 <wangqi80@meituan.com>
2020-10-05 19:15:57 +0800

    feat: add a utility method `Utils.gem_installed?` to check whether the specified gem is installed.

--- utils.rb
+++ utils.rb
@@ -13,5 +13,18 @@ module Utils
     def command_exist?(name)
       system("command -v #{name} >/dev/null 2>&1")
     end
+
+    # Check whether the specified gem is installed.
+    #
+    # @param [String] gem_name
+    #
+    # @return [Boolean]
+    #         `true` if installed; `false` otherwise.
+    #
+    def gem_installed?(gem_name)
+      return false if gem_name.nil? || gem_name.empty?
+
+      `gem list --installed #{gem_name}`.chomp.downcase == 'true'
+    end
   end
 end
